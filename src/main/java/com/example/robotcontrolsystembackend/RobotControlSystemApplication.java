df3d9d8804c619226c37b98cd38defb0d8674051
Khoa-213 <khoahass181074@fpt.edu.vn>
2026-01-26 20:34:20 +0700

    Add project structure

---
+++ src/main/java/com/example/robotcontrolsystembackend/RobotControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.robotcontrolsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotControlSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/robotcontrolsystembackend/RobotControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.robotcontrolsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
