dc4dc7e8b6960895243065240bf95b89e8e7ccdf
Nhahnt <trongnha140204@gmail.com>
2026-01-26 21:59:55 -0500

    Test git Nhã

--- src/main/java/com/example/robotcontrolsystembackend/repository/FactoryRepository.java
+++ src/main/java/com/example/robotcontrolsystembackend/repository/FactoryRepository.java
@@ -4,6 +4,4 @@ import com.example.robotcontrolsystembackend.domain.entity.Factory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FactoryRepository extends JpaRepository<Factory, Long> {
-
-    
 }
