74db3582dd2e6b4507f20521bac4d79d9a548b78
Khoa-213 <khoahass181074@fpt.edu.vn>
2026-01-28 20:29:32 +0700

    Cấu trúc Pakage và Code Enum

--- src/main/java/com/example/robotcontrolsystembackend/presentation/controller/factory/FactoryController.java
+++ src/main/java/com/example/robotcontrolsystembackend/presentation/controller/factory/FactoryController.java
@@ -1,29 +1,11 @@
 package com.example.robotcontrolsystembackend.presentation.controller.factory;
 
-import com.example.robotcontrolsystembackend.domain.model.Factory;
-import com.example.robotcontrolsystembackend.repository.FactoryRepository;
 import org.springframework.web.bind.annotation.* ;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/factories")
 public class FactoryController {
 
-    private final FactoryRepository factoryRepository;//
-
-    public FactoryController(FactoryRepository factoryRepository) {
-        this.factoryRepository = factoryRepository;
-    }
-
-    @GetMapping
-    public List<Factory> getAll() {
-        return factoryRepository.findAll();
-    }
-
-    @PostMapping
-    public Factory create(@RequestBody Factory factory) {
-        // factoryId sẽ auto-generate
-        return factoryRepository.save(factory);
-    }
 }
