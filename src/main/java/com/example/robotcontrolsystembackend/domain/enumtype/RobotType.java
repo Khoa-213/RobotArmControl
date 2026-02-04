da3d74ae2cc52c3a45dcf5e2cf6e3027b9949cb5
Khoa-213 <khoahass181074@fpt.edu.vn>
2026-02-04 23:18:21 +0700

    Change enum RobotType

--- src/main/java/com/example/robotcontrolsystembackend/domain/enumtype/RobotType.java
+++ src/main/java/com/example/robotcontrolsystembackend/domain/enumtype/RobotType.java
@@ -1,13 +1,6 @@
 package com.example.robotcontrolsystembackend.domain.enumtype;
 
 public enum RobotType {
-    ARTICULATED,      // Robot khớp nối (6 trục)
-    SCARA,            // Robot SCARA
-    DELTA,            // Robot Delta
-    CARTESIAN,        // Robot Cartesian (tọa độ Đề-các)
-    CYLINDRICAL,      // Robot hình trụ
-    POLAR,            // Robot cực
-    COLLABORATIVE,    // Robot cộng tác (Cobot)
-    MOBILE,           // Robot di động
-    HUMANOID          // Robot hình người
+    Unity,
+    Real// Robot hình người
 }
